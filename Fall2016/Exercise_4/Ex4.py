d97a9363bd1dc74e0794d6e8e63b317d05d64dfd
Mihai Popa <mihai.popa1201@gmail.com>
2016-11-17 14:16:47 +0000

    Fix code for question b

--- Fall2016/Exercise_4/Ex4.py
+++ Fall2016/Exercise_4/Ex4.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random
 from IzNetwork import *
-from pylab import plot, show
+from pylab import plot, show, savefig
+import matplotlib.pyplot as plt
 
 class ModularNetwork(object):
     def __init__(self, NExcitatoryModules, NExcitatory, NInhibitory, p):
@@ -14,7 +15,7 @@ class ModularNetwork(object):
     def setParameters(self):
         a = [0.02] * (self._NExcitatory + self._NInhibitory)
         b = [0.2] * self._NExcitatory + [0.25] * self._NInhibitory
-        c = [65] * (self._NExcitatory + self._NInhibitory)
+        c = [-65] * (self._NExcitatory + self._NInhibitory)
         d = [8] * self._NExcitatory + [2] * self._NInhibitory
         self._network.setParameters(np.asarray(a), np.asarray(b), np.asarray(c), np.asarray(d))
 
@@ -33,9 +34,9 @@ class ModularNetwork(object):
                     if random.random() < self._p:
                         while True:
                             module = random.sample(range(0, i) + range(i + 1, self._NExcitatoryModules), 1)[0]
-                            if module == i:
-                                continue
                             z = module * perModule + random.randint(0, perModule - 1)
+                            if module == i or z == x:
+                                continue
                             if self._edges[x][z] == 0:
                                 self._edges[x][z] = 1 
                                 break
@@ -50,17 +51,30 @@ class ModularNetwork(object):
         # Connect inhibitory with everyone else
         for i in range(self._NExcitatory, self._NTotal):
             for j in range(self._NTotal):
-              self._edges[i][j] = 1
+                if i != j:
+                    self._edges[i][j] = 1
               
     def isExcitatory(self, i):
         return i < self._NExcitatory
 
     def _getWeight(self, i, j):
         if self.isExcitatory(i) and self.isExcitatory(j):
+            return 1.0 * 17
+        if self.isExcitatory(i):
+            return random.random() * 50
+        if not self.isExcitatory(j):
+            return -random.random()
+        return -random.random() * 2
+        if self.isExcitatory(i) and self.isExcitatory(j):
             return 1
         if self.isExcitatory(i):
             return random.random()
         return -random.random()
+        # if self.isExcitatory(i) and self.isExcitatory(j):
+        #     return 1
+        # if self.isExcitatory(i):
+        #     return random.random()
+        # return -random.random()
 
     def setWeights(self):
         self._weights = np.zeros((self._NTotal, self._NTotal))
@@ -68,6 +82,7 @@ class ModularNetwork(object):
             for j in range(self._NTotal):
                 if self._edges[i][j] != 0:
                     self._weights[i][j] = self._getWeight(i, j)
+        self._network.setWeights(self._weights)
     
     def setDelays(self):
         delays = np.zeros((self._NTotal, self._NTotal), dtype=int)
@@ -86,23 +101,36 @@ class ModularNetwork(object):
         self.generateEdges()
         self.setWeights()
         self.setDelays()
-        
+
     def plotA(self):
         for x in range(self._NTotal):
-            print x
             for y in range(self._NTotal):
                 if self._edges[x][y] != 0:
                     plot(x, y, '.')
-        show()
+        savefig("a.png")
+
+    def generatePoissonCurrent(self):
+        current = np.zeros(self._NTotal)
+        samples = np.random.poisson(0.01, self._NTotal)
+        for i in range(self._NTotal):
+            if samples[i] > 0:
+                current[i] = 15
+        return current
+        
+    def plotB(self):
+        fireEvents = []
+        for time in range(1000):
+            self._network.setCurrent(self.generatePoissonCurrent())
+            for neuron in self._network.update():
+                plot(time, neuron, '.')
+        savefig("b.png")
 
 def main():
-    network = ModularNetwork(8, 800, 200, 0.1)
+    network = ModularNetwork(8, 800, 200, 0)
     network.prepare()
     # network.plotA()
-    results = []
-    for i in range(1000):
-        results.append(network._network.update())
-    print(results)
+    # network.plotB()
+    # network.plotC()
 
 if __name__ == "__main__":
     main()
